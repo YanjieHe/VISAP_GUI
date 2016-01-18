475ed771db8667af6ba51b9bc4dbfdea7de2837c
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-18 11:43:54 +0800

    add

--- AddVariable.cs
+++ AddVariable.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace 统计图形界面1
 {
     public partial class AddVariable : Form
@@ -17,33 +16,13 @@ namespace 统计图形界面1
             InitializeComponent();
         }
 
-        public static Form1 form1 = null;
+        
         private void button_add_Click(object sender, EventArgs e)
         {
-            try{
-                //MessageBox.Show(((DataGridView)Application.OpenForms["Form1"].Controls["dataGridView1"]).Rows.Count + "");
-                int ColumnCounts = ((DataGridView)Application.OpenForms["Form1"].Controls["dataGridView1"]).Columns.Count;
-                
-                using (DataTable dtable = ((DataGridView)Application.OpenForms["Form1"].Controls["dataGridView1"]).DataSource as DataTable)
-                {
-                    //((DataGridView)Application.OpenForms["Form1"].Controls["dataGridView1"]).Columns.Add(textBox1.Text.ToString());
-                    dtable.Columns.Add(textBox1.Text.ToString());
-                    ((DataGridView)Application.OpenForms["Form1"].Controls["dataGridView1"]).DataSource = dtable;
-                }
-                /*if (form1.dataGridView1.DataSource != null)
-                {
-                    int ColumnCounts = form1.dataGridView1.Columns.Count;
-                    using (DataTable dtable = form1.dataGridView1.DataSource as DataTable)
-                    {
-                        dtable.Columns.Add(textBox1.Text.ToString());
-                        form1.dataGridView1.DataSource = dtable;
-                    }
-                }*/
-            }
-                catch (Exception ex)
+            if (textBox1.Text.ToString() != null)
             {
-
-                }
+                Form1.S.AddOneColumn(textBox1.Text.ToString());
+            }
             }
         
     }
