a70202d3657b8d9b87402ce1c745c2fc0690dbd1
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-20 14:44:25 +0800

    add

---
+++ BasicGraph.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace 统计图形界面1
+{
+    partial class BasicGraph
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart_basic = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ChosenCols = new System.Windows.Forms.TextBox();
+            this.button_import = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button_AddPlot = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_copy = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_basic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart_basic
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_basic.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_basic.Legends.Add(legend1);
+            this.chart_basic.Location = new System.Drawing.Point(274, 48);
+            this.chart_basic.Name = "chart_basic";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart_basic.Series.Add(series1);
+            this.chart_basic.Size = new System.Drawing.Size(677, 428);
+            this.chart_basic.TabIndex = 0;
+            this.chart_basic.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(220, 287);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(15, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "y:";
+            // 
+            // textBox_ChosenCols
+            // 
+            this.textBox_ChosenCols.Location = new System.Drawing.Point(18, 119);
+            this.textBox_ChosenCols.Name = "textBox_ChosenCols";
+            this.textBox_ChosenCols.Size = new System.Drawing.Size(214, 21);
+            this.textBox_ChosenCols.TabIndex = 11;
+            this.textBox_ChosenCols.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button_import
+            // 
+            this.button_import.Location = new System.Drawing.Point(157, 152);
+            this.button_import.Name = "button_import";
+            this.button_import.Size = new System.Drawing.Size(75, 23);
+            this.button_import.TabIndex = 12;
+            this.button_import.Text = "导入数据";
+            this.button_import.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 20);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(46, 48);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(186, 20);
+            this.comboBox2.TabIndex = 14;
+            // 
+            // button_AddPlot
+            // 
+            this.button_AddPlot.Location = new System.Drawing.Point(372, 11);
+            this.button_AddPlot.Name = "button_AddPlot";
+            this.button_AddPlot.Size = new System.Drawing.Size(75, 23);
+            this.button_AddPlot.TabIndex = 1;
+            this.button_AddPlot.Text = "添加";
+            this.button_AddPlot.UseVisualStyleBackColor = true;
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(518, 11);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 2;
+            this.button_clear.Text = "清空";
+            this.button_clear.UseVisualStyleBackColor = true;
+            // 
+            // button_copy
+            // 
+            this.button_copy.Location = new System.Drawing.Point(664, 11);
+            this.button_copy.Name = "button_copy";
+            this.button_copy.Size = new System.Drawing.Size(75, 23);
+            this.button_copy.TabIndex = 3;
+            this.button_copy.Text = "复制";
+            this.button_copy.UseVisualStyleBackColor = true;
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(810, 11);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 15;
+            this.button_save.Text = "保存";
+            this.button_save.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "散点图",
+            "折线图"});
+            this.comboBox3.Location = new System.Drawing.Point(90, 87);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(142, 20);
+            this.comboBox3.TabIndex = 16;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(15, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "图表类型:";
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Location = new System.Drawing.Point(67, 151);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(75, 23);
+            this.button_refresh.TabIndex = 18;
+            this.button_refresh.Text = "刷新列数";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // BasicGraph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 485);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button_import);
+            this.Controls.Add(this.textBox_ChosenCols);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_copy);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_AddPlot);
+            this.Controls.Add(this.chart_basic);
+            this.Name = "BasicGraph";
+            this.Text = "基本图表";
+            this.Load += new System.EventHandler(this.BasicGraph_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart_basic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_basic;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ChosenCols;
+        private System.Windows.Forms.Button button_import;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button_AddPlot;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_copy;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_refresh;
+    }
+}
---
+++ BasicGraph.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 统计图形界面1
+{
+    public partial class BasicGraph : Form
+    {
+        public BasicGraph()
+        {
+            InitializeComponent();
+            textBox_ChosenCols.Text = RenewCols();
+        }
+        static int[] ColumnsSelected()
+        {
+            
+             int selectedCellCount =Form1.S.dataGridView1.GetCellCount(DataGridViewElementStates.Selected);
+             
+             if (selectedCellCount > 0)
+             {
+                 int[] ColumnsChosen = new int[selectedCellCount];
+                 if (Form1.S.dataGridView1.AreAllCellsSelected(true))
+                 {
+                     for (int i = 0; i < Form1.S.dataGridView1.Columns.Count; i++)
+                     {
+                         ColumnsChosen[i] = i;
+                     }
+                    
+                 }
+                 else
+                 {
+                     for (int i = 0;i < selectedCellCount; i++)
+                     {
+                         ColumnsChosen[i] = Form1.S.dataGridView1.SelectedCells[i].ColumnIndex;
+                     }
+                     
+                 }
+                 return ColumnsChosen;
+             }
+             return new int[] { -1, -1, -1, -1 };
+        }
+        private float X;
+        private float Y;
+        private void setTag(Control cons)
+        {
+            //遍历窗体中的控件
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                    setTag(con);
+            }
+        }
+        private void setControls(float newx, float newy, Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls)
+            {
+                string[] mytag = con.Tag.ToString().Split(new char[] { ':' });//获取控件的Tag属性值，并分割后存储字符串数组
+                float a = Convert.ToSingle(mytag[0]) * newx;//根据窗体缩放比例确定控件的值，宽度
+                con.Width = (int)a;//宽度
+                a = Convert.ToSingle(mytag[1]) * newy;//高度
+                con.Height = (int)(a);
+                a = Convert.ToSingle(mytag[2]) * newx;//左边距离
+                con.Left = (int)(a);
+                a = Convert.ToSingle(mytag[3]) * newy;//上边缘距离
+                con.Top = (int)(a);
+                Single currentSize = Convert.ToSingle(mytag[4]) * newy;//字体大小
+                con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                if (con.Controls.Count > 0)
+                {
+                    setControls(newx, newy, con);
+                }
+            }
+        }
+        private void BasicGraph_Load(object sender, EventArgs e)
+        {
+            this.Resize += new EventHandler(BasicGraph_Resize);//窗体调整大小时引发事件
+            X = this.Width;//获取窗体的宽度
+            Y = this.Height;//获取窗体的高度
+            setTag(this);//调用方法
+        }
+        void BasicGraph_Resize(object sender, EventArgs e)
+        {
+            float newx = (this.Width) / X; //窗体宽度缩放比例
+            float newy = this.Height / Y;//窗体高度缩放比例
+            setControls(newx, newy, this);//随窗体改变控件大小
+            //this.Text = this.Width.ToString() + " " + this.Height.ToString();//窗体标题栏文本
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        static string RenewCols()
+        {
+            int []  ColumnsChosen= new int [Form1.S.dataGridView1.Columns.Count];
+            ColumnsChosen = ColumnsSelected();
+            string AllSelectedCol = "";
+            if (ColumnsChosen != new int[] { -1, -1, -1, -1 })
+            {
+                Array.Sort(ColumnsChosen);
+                for (int i = 0; i < ColumnsChosen.Length; i++)
+                {
+                    if (i == 0){
+                        AllSelectedCol = (ColumnsChosen[i]+1).ToString();
+                    }
+                    else
+                    {
+                        //只有第一列有可能为0，此后每列都不可能为0
+                        if (ColumnsChosen[i]!= 0)
+                        AllSelectedCol = AllSelectedCol + "," + (ColumnsChosen[i]+1).ToString();
+                    }
+                    
+                }
+            }
+            return AllSelectedCol;
+        }
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+                textBox_ChosenCols.Text = RenewCols();
+            } 
+        }
+    }
+
