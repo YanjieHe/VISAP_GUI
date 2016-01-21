e2adb5f0571aa2954a6662fa7fdf9ebc989acf19
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-21 15:10:54 +0800

    add

---
+++ ParameterEstimation.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace 统计图形界面1
+{
+    partial class SingleParameterEstimation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_estimate = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Cols = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_ColShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_estimate
+            // 
+            this.button_estimate.Location = new System.Drawing.Point(587, 260);
+            this.button_estimate.Name = "button_estimate";
+            this.button_estimate.Size = new System.Drawing.Size(230, 93);
+            this.button_estimate.TabIndex = 0;
+            this.button_estimate.Text = "估计";
+            this.button_estimate.UseVisualStyleBackColor = true;
+            this.button_estimate.Click += new System.EventHandler(this.button_estimate_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "均值",
+            "比例"});
+            this.comboBox2.Location = new System.Drawing.Point(721, 185);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(96, 20);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(564, 341);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(587, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "待估计参数：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(583, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "列数：";
+            // 
+            // textBox_Cols
+            // 
+            this.textBox_Cols.Location = new System.Drawing.Point(587, 41);
+            this.textBox_Cols.Multiline = true;
+            this.textBox_Cols.Name = "textBox_Cols";
+            this.textBox_Cols.Size = new System.Drawing.Size(230, 48);
+            this.textBox_Cols.TabIndex = 7;
+            this.textBox_Cols.TextChanged += new System.EventHandler(this.textBox_Cols_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(587, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "变量名：";
+            // 
+            // textBox_ColShow
+            // 
+            this.textBox_ColShow.Location = new System.Drawing.Point(587, 125);
+            this.textBox_ColShow.Multiline = true;
+            this.textBox_ColShow.Name = "textBox_ColShow";
+            this.textBox_ColShow.ReadOnly = true;
+            this.textBox_ColShow.Size = new System.Drawing.Size(230, 48);
+            this.textBox_ColShow.TabIndex = 9;
+            // 
+            // SingleParameterEstimation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 388);
+            this.Controls.Add(this.textBox_ColShow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Cols);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button_estimate);
+            this.Name = "SingleParameterEstimation";
+            this.Text = "SingleParameterEstimation";
+            this.Load += new System.EventHandler(this.SingleParameterEstimation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_estimate;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Cols;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_ColShow;
+    }
+}
---
+++ ParameterEstimation.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 统计图形界面1
+{
+    public partial class SingleParameterEstimation : Form
+    {
+        public SingleParameterEstimation()
+        {
+            InitializeComponent();
+            textBox_Cols.Text = RenewCol();
+        }
+
+        private void SingleParameterEstimation_Load(object sender, EventArgs e)
+        {
+
+        }
+        static string RenewCol()
+        {
+            int selectedCellCount = Form1.S.dataGridView1.GetCellCount(DataGridViewElementStates.Selected);
+            int[] ColumnsChosen = new int[selectedCellCount];
+            ColumnsChosen = ColumnsSelected();
+            string AllSelectedCol = "";
+            int Record_Zero = 0;
+            if (ColumnsChosen != new int[] { -1, -1, -1, -1 })
+            {
+                //MessageBox.Show("selectedCellCount = " + selectedCellCount);
+                Array.Sort(ColumnsChosen);
+                for (int i = 0; i < selectedCellCount; i++)
+                {
+                    //MessageBox.Show("ColumnsChosen[i]"+ColumnsChosen[i]);
+                    if (ColumnsChosen[i] >= 0)
+                    {
+                        Record_Zero = i;
+                        break;
+                    }
+                }
+                //MessageBox.Show("i = " + Record_Zero );
+
+                for (int i = Record_Zero; i < selectedCellCount; i++)
+                {
+                    if (i == Record_Zero)
+                    {
+                        AllSelectedCol = (ColumnsChosen[i] + 1).ToString();
+
+                    }
+                    else
+                    {
+                        AllSelectedCol = AllSelectedCol + "," + (ColumnsChosen[i] + 1).ToString();
+                    }
+
+                }
+
+            }
+            return AllSelectedCol;
+        }
+        static int[] ColumnsSelected()
+        {
+            int counts = 0;
+            int UseToIdentify = 0;
+            int countforNew = 0;
+            int selectedCellCount = Form1.S.dataGridView1.GetCellCount(DataGridViewElementStates.Selected);
+            if (selectedCellCount > 0)
+            {
+                int[] ColumnsChosen = new int[selectedCellCount];
+                for (int i = 0; i < selectedCellCount; i++)
+                {
+                    ColumnsChosen[i] = -100;
+                }
+                if (Form1.S.dataGridView1.AreAllCellsSelected(true))
+                {
+                    for (int i = 0; i < Form1.S.dataGridView1.Columns.Count; i++)
+                    {
+                        counts = 0;
+                        foreach (int EachCol in ColumnsChosen)
+                        {
+                            if (EachCol == i)
+                            {
+                                counts++;
+                                break;
+                            }
+                        }
+                        if (counts == 0)
+                        {
+                            ColumnsChosen[i] = i;
+
+                        }
+                        else
+                        {
+                            ColumnsChosen[i] = -100;
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    for (int i = 0; i < selectedCellCount; i++)
+                    {
+                        UseToIdentify = Form1.S.dataGridView1.SelectedCells[i].ColumnIndex;
+                        counts = 0;
+                        foreach (int EachCol in ColumnsChosen)
+                        {
+                            if (EachCol == UseToIdentify)
+                            {
+                                counts++;
+                                break;
+                            }
+                        }
+                        if (counts == 0)
+                        {
+                            ColumnsChosen[countforNew] = UseToIdentify;
+                            countforNew++;
+                        }
+                        else
+                        {
+                            ColumnsChosen[countforNew] = -100;
+                            countforNew++;
+                        }
+                    }
+
+                }
+                return ColumnsChosen;
+            }
+            return new int[] { -1, -1, -1, -1 };
+        }
+        
+        private void textBox_Cols_TextChanged(object sender, EventArgs e)
+        {
+            string All_Col_Selected = textBox_Cols.Text;
+            char[] separator = { ',' };
+            string[] Col_Selected = All_Col_Selected.Split(separator);
+            string ColToShow = "";
+            foreach (string Col in Col_Selected)
+            {
+
+                if (MathV.IsStringInt(Col.Trim()) == 1 && Col.Trim()!= "")
+                {
+                    if (Convert.ToInt32(Col.Trim()) >= 0 && Convert.ToInt32(Col.Trim())<= Form1.S.dataGridView1.Columns.Count)
+                    ColToShow = ColToShow+","+Form1.S.dataGridView1.Columns[Convert.ToInt32(Col.Trim())-1].Name;
+                }
+            }
+            if (ColToShow != "")
+            {
+                ColToShow = ColToShow.Substring(1, ColToShow.Length - 1);
+            }
+            textBox_ColShow.Text = ColToShow;
+        }
+        string FindNAs(int ColNum)
+        {
+            int AllRowCounts = Form1.S.dataGridView1.RowCount;
+            string WarningNAs = " ";
+            int CountNA = 0;
+            for (int i = 0; i < AllRowCounts; i++)
+            {
+                if (Form1.S.dataGridView1.Rows[i].Cells[ColNum].Value == null)
+                {
+                    WarningNAs = WarningNAs + "," + i.ToString();
+                    CountNA++;
+                }
+                else if (Form1.S.dataGridView1.Rows[i].Cells[ColNum].Value.ToString().Trim() == "")
+                {
+                    WarningNAs = WarningNAs + "," + i.ToString();
+                    CountNA++;
+                }
+                else
+                {
+                    WarningNAs = WarningNAs + ",NOEMPTY";
+                }
+            }
+            return WarningNAs;
+        }
+        BigNumber [] VectorReadASBigNum(string ID, string WarningNAs)
+        {
+            //读单列
+            int ColNum = 0;
+            for (int i = 0; i < Form1.S.dataGridView1.ColumnCount; i++)
+            {
+                if (Form1.S.dataGridView1.Columns[i].Name == ID)
+                {
+                    ColNum = i;
+                    break;
+                }
+            }
+            int AllRowCounts = Form1.S.dataGridView1.RowCount - 1;
+            char[] separator = { ',' };
+            int IsNOTEmpty = 0;
+            string Temp;
+            foreach (char EachChar in WarningNAs)
+            {
+                if (EachChar == 'N')
+                {
+                    IsNOTEmpty++;
+                }
+            }
+            string[] SkipRow = WarningNAs.Split(separator);
+            int DataRows_Count = 0;
+            int IsEmpty = 0;
+            BigNumber [] DataChose = new BigNumber [IsNOTEmpty];
+            for (int i = 0; i < AllRowCounts; i++)
+            {
+                IsEmpty = 0;
+                foreach (string EachRow in SkipRow)
+                {
+                    if (EachRow.Trim() == i.ToString())
+                    {
+                        IsEmpty = 1;
+                    }
+                }
+                if (IsEmpty == 0)
+                {
+                    Temp =Form1.S.dataGridView1.Rows[i].Cells[ColNum].Value.ToString();
+                    //MessageBox.Show("第" + i + "行的变量的值为：" + Temp);
+                    DataChose[DataRows_Count] = new BigNumber(Temp.Trim());
+                    DataRows_Count++;
+                }
+            }
+            return DataChose;
+
+        }
+        private void button_estimate_Click(object sender, EventArgs e)
+        {
+            string All_Col_Selected = textBox_Cols.Text;
+            char[] separator = { ',' };
+            string[] Col_Selected = All_Col_Selected.Split(separator);
+            int [] Para_Esti_Col  = new int[Col_Selected.Length];
+            int Counts = 0;
+            foreach (string Col in Col_Selected)
+            {
+
+                if (MathV.IsStringInt(Col.Trim()) == 1 && Col.Trim()!= "")
+                {
+                    if (Convert.ToInt32(Col.Trim()) >= 0 && Convert.ToInt32(Col.Trim())<= Form1.S.dataGridView1.Columns.Count){
+                    Para_Esti_Col[Counts] = Convert.ToInt32(Col.Trim());
+                        Counts++;
+                    }
+                    else{
+                        Para_Esti_Col[Counts]  = -100;
+                        Counts++;
+                    }
+                }
+            }
+            for (int i = 0; i < Counts; i++)
+            {
+                if (Para_Esti_Col[i] >= 0)
+                {
+
+                }
+            }
+        }
+    }
+}
