e047e89364657d117c70a494081f3cd84179e0be
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-22 20:55:49 +0800

    add

---
+++ FunctionPlot.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace 统计图形界面1
+{
+    partial class FunctionPlot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart_func = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_StartValue = new System.Windows.Forms.TextBox();
+            this.textBox_Step = new System.Windows.Forms.TextBox();
+            this.textBox_EndValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_Function = new System.Windows.Forms.TextBox();
+            this.button_plot = new System.Windows.Forms.Button();
+            this.button_ImportReport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_func)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart_func
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart_func.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart_func.Legends.Add(legend2);
+            this.chart_func.Location = new System.Drawing.Point(12, 83);
+            this.chart_func.Name = "chart_func";
+            this.chart_func.Size = new System.Drawing.Size(815, 371);
+            this.chart_func.TabIndex = 0;
+            this.chart_func.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x：";
+            // 
+            // textBox_StartValue
+            // 
+            this.textBox_StartValue.Location = new System.Drawing.Point(122, 12);
+            this.textBox_StartValue.Name = "textBox_StartValue";
+            this.textBox_StartValue.Size = new System.Drawing.Size(67, 21);
+            this.textBox_StartValue.TabIndex = 2;
+            // 
+            // textBox_Step
+            // 
+            this.textBox_Step.Location = new System.Drawing.Point(418, 12);
+            this.textBox_Step.Name = "textBox_Step";
+            this.textBox_Step.Size = new System.Drawing.Size(67, 21);
+            this.textBox_Step.TabIndex = 3;
+            // 
+            // textBox_EndValue
+            // 
+            this.textBox_EndValue.Location = new System.Drawing.Point(270, 12);
+            this.textBox_EndValue.Name = "textBox_EndValue";
+            this.textBox_EndValue.Size = new System.Drawing.Size(67, 21);
+            this.textBox_EndValue.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(343, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "步长：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(195, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "终值：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(47, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "初值：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(12, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "y =";
+            // 
+            // textBox_Function
+            // 
+            this.textBox_Function.Location = new System.Drawing.Point(77, 45);
+            this.textBox_Function.Name = "textBox_Function";
+            this.textBox_Function.Size = new System.Drawing.Size(408, 21);
+            this.textBox_Function.TabIndex = 9;
+            // 
+            // button_plot
+            // 
+            this.button_plot.Location = new System.Drawing.Point(566, 12);
+            this.button_plot.Name = "button_plot";
+            this.button_plot.Size = new System.Drawing.Size(252, 23);
+            this.button_plot.TabIndex = 10;
+            this.button_plot.Text = "绘图";
+            this.button_plot.UseVisualStyleBackColor = true;
+            this.button_plot.Click += new System.EventHandler(this.button_plot_Click);
+            // 
+            // button_ImportReport
+            // 
+            this.button_ImportReport.Location = new System.Drawing.Point(566, 41);
+            this.button_ImportReport.Name = "button_ImportReport";
+            this.button_ImportReport.Size = new System.Drawing.Size(252, 23);
+            this.button_ImportReport.TabIndex = 11;
+            this.button_ImportReport.Text = "导入报告";
+            this.button_ImportReport.UseVisualStyleBackColor = true;
+            // 
+            // FunctionPlot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 466);
+            this.Controls.Add(this.button_ImportReport);
+            this.Controls.Add(this.button_plot);
+            this.Controls.Add(this.textBox_Function);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_EndValue);
+            this.Controls.Add(this.textBox_Step);
+            this.Controls.Add(this.textBox_StartValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart_func);
+            this.Name = "FunctionPlot";
+            this.Text = "FunctionPlot";
+            ((System.ComponentModel.ISupportInitialize)(this.chart_func)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_func;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_StartValue;
+        private System.Windows.Forms.TextBox textBox_Step;
+        private System.Windows.Forms.TextBox textBox_EndValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_Function;
+        private System.Windows.Forms.Button button_plot;
+        private System.Windows.Forms.Button button_ImportReport;
+    }
+}
---
+++ FunctionPlot.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace 统计图形界面1
+{
+    public partial class FunctionPlot : Form
+    {
+        public FunctionPlot()
+        {
+            InitializeComponent();
+        }
+        private object ComplierCode(string expression)
+        {
+            string code = WrapExpression(expression);
+
+            CSharpCodeProvider csharpCodeProvider = new CSharpCodeProvider();
+
+            //编译的参数
+            CompilerParameters compilerParameters = new CompilerParameters();
+
+            compilerParameters.CompilerOptions = "/t:library";
+            compilerParameters.GenerateInMemory = true;
+            //开始编译
+            CompilerResults compilerResults = csharpCodeProvider.CompileAssemblyFromSource(compilerParameters, code);
+            if (compilerResults.Errors.Count > 0)
+                throw new Exception("编译出错！");
+
+            Assembly assembly = compilerResults.CompiledAssembly;
+            Type type = assembly.GetType("ExpressionCalculate");
+            MethodInfo method = type.GetMethod("Calculate");
+            return method.Invoke(null, null);
+        }
+
+        private string WrapExpression(string expression)
+        {
+            string code = @"
+                using System;
+                class ExpressionCalculate
+                {
+            
+                    public static object Calculate()
+                    {
+                        {0};
+                    }
+                }
+            ";
+
+            return code.Replace("{0}", expression);
+        }
+        public static string StringReplace(string str, string toRep, string strRep)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int np = 0, n_ptmp = 0;
+
+            for (; ; )
+            {
+                string str_tmp = str.Substring(np);
+                n_ptmp = str_tmp.IndexOf(toRep);
+
+                if (n_ptmp == -1)
+                {
+                    sb.Append(str_tmp);
+                    break;
+                }
+                else
+                {
+                    sb.Append(str_tmp.Substring(0, n_ptmp)).Append(strRep);
+                    np += n_ptmp + toRep.Length;
+                }
+            }
+            return sb.ToString();
+
+        }
+        void  FuncRead(double StartValue, double EndValue,double Step)
+        {
+           /* string PlotCode = @"string Answer = "";";
+            PlotCode += "for (double i = StartValue;i < EndValue;i = i + Step)";
+            PlotCode += "{Answer += ";
+            PlotCode +=," + Expression;}";*/
+            string PlotCode = @"
+                              string Answer = {4};
+                               for (double x = {0};x < {1};x = x + {2}){
+                               Answer += "","" + ({3}).ToString();}return Answer;
+        
+            ";
+            PlotCode = PlotCode.Replace("{0}", StartValue.ToString());
+            PlotCode = PlotCode.Replace("{1}", EndValue.ToString());
+            PlotCode = PlotCode.Replace("{2}", Step.ToString());
+            PlotCode = PlotCode.Replace("{4}", "\"\"");
+            string FunctionExpression = textBox_Function.Text;
+            FunctionExpression = FunctionExpression.Trim().ToLower();
+            FunctionExpression = StringReplace(FunctionExpression, "sqrt", "Math.Sqrt");
+            FunctionExpression = StringReplace(FunctionExpression, "pow", "Math.Pow");
+            FunctionExpression = StringReplace(FunctionExpression, "exp", "Math.Exp");
+            FunctionExpression = StringReplace(FunctionExpression, "log", "Math.Log10");
+            FunctionExpression = StringReplace(FunctionExpression, "sin", "Math.Sin");
+            FunctionExpression = StringReplace(FunctionExpression, "cos", "Math.Cos");
+            FunctionExpression = StringReplace(FunctionExpression, "tan", "Math.Tan");
+            FunctionExpression = StringReplace(FunctionExpression, "pi", "Math.PI");
+            //MessageBox.Show("函数表达式 = " + FunctionExpression);
+            PlotCode = PlotCode.Replace("{3}", FunctionExpression.ToString());
+            //MessageBox.Show("PlotCode = " + PlotCode);
+            string Result = "";
+            
+            try
+            {
+                string expression = PlotCode.Trim();
+                //MessageBox.Show("Expression = " + expression);
+                Result = this.ComplierCode(expression).ToString();
+                //MessageBox.Show("Result = " + Result);
+                Result = Result.Substring(1, Result.Length - 1);
+                FuncPlotting(Result);
+                
+
+            }
+            catch (Exception ex)
+            {
+                //txtResult.Text = ex.Message;
+                MessageBox.Show("出错");
+            }
+        }
+        void FuncPlotting(string Result)
+        {
+            char[] separator = { ',' };
+            string[] AllResults = Result.Split(separator);
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Spline;
+            double StartValue = Convert.ToDouble(textBox_StartValue.Text);
+            double EndValue = Convert.ToDouble(textBox_EndValue.Text);
+            double Step = Convert.ToDouble(textBox_Step.Text);
+            for (double x = StartValue ,i = 0; x < EndValue; x = x + Step,i++)
+            {
+                series.Points.AddXY(x, Convert.ToDouble(AllResults[Convert.ToInt32(i)]));
+            }
+            chart_func.Series.Add(series);
+
+        }
+        private void button_plot_Click(object sender, EventArgs e)
+        {
+            FuncRead(Convert.ToDouble(textBox_StartValue.Text), Convert.ToDouble(textBox_EndValue.Text), Convert.ToDouble(textBox_Step.Text));
+        }
+    }
+}
