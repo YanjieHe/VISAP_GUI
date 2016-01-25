61c4e366aaf7e04d062b0486913088af3c16385a
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-25 14:47:25 +0800

    add

---
+++ BayesEstimation.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace 统计图形界面1
+{
+    partial class BayesEstimation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart_prior = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart_posterior = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_PriorVariance = new System.Windows.Forms.TextBox();
+            this.textBox_PriorMean = new System.Windows.Forms.TextBox();
+            this.button_GeneratePrior = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_prior)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_posterior)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart_prior
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart_prior.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart_prior.Legends.Add(legend5);
+            this.chart_prior.Location = new System.Drawing.Point(3, 7);
+            this.chart_prior.Name = "chart_prior";
+            this.chart_prior.Size = new System.Drawing.Size(500, 369);
+            this.chart_prior.TabIndex = 0;
+            this.chart_prior.Text = "chart1";
+            // 
+            // chart_posterior
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart_posterior.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart_posterior.Legends.Add(legend6);
+            this.chart_posterior.Location = new System.Drawing.Point(559, 7);
+            this.chart_posterior.Name = "chart_posterior";
+            this.chart_posterior.Size = new System.Drawing.Size(500, 369);
+            this.chart_posterior.TabIndex = 1;
+            this.chart_posterior.Text = "chart2";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 402);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(410, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(130, 436);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(82, 21);
+            this.textBox_a.TabIndex = 3;
+            this.textBox_a.Text = "5";
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(322, 436);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(82, 21);
+            this.textBox_b.TabIndex = 4;
+            this.textBox_b.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(78, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(287, 438);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(221, 465);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "先验方差:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(12, 465);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "先验均值:";
+            // 
+            // textBox_PriorVariance
+            // 
+            this.textBox_PriorVariance.Location = new System.Drawing.Point(322, 463);
+            this.textBox_PriorVariance.Name = "textBox_PriorVariance";
+            this.textBox_PriorVariance.ReadOnly = true;
+            this.textBox_PriorVariance.Size = new System.Drawing.Size(82, 21);
+            this.textBox_PriorVariance.TabIndex = 8;
+            // 
+            // textBox_PriorMean
+            // 
+            this.textBox_PriorMean.Location = new System.Drawing.Point(130, 463);
+            this.textBox_PriorMean.Name = "textBox_PriorMean";
+            this.textBox_PriorMean.ReadOnly = true;
+            this.textBox_PriorMean.Size = new System.Drawing.Size(82, 21);
+            this.textBox_PriorMean.TabIndex = 7;
+            // 
+            // button_GeneratePrior
+            // 
+            this.button_GeneratePrior.Location = new System.Drawing.Point(428, 438);
+            this.button_GeneratePrior.Name = "button_GeneratePrior";
+            this.button_GeneratePrior.Size = new System.Drawing.Size(75, 23);
+            this.button_GeneratePrior.TabIndex = 11;
+            this.button_GeneratePrior.Text = "生成先验";
+            this.button_GeneratePrior.UseVisualStyleBackColor = true;
+            this.button_GeneratePrior.Click += new System.EventHandler(this.button_GeneratePrior_Click);
+            // 
+            // BayesEstimation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 492);
+            this.Controls.Add(this.button_GeneratePrior);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_PriorVariance);
+            this.Controls.Add(this.textBox_PriorMean);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.chart_posterior);
+            this.Controls.Add(this.chart_prior);
+            this.Name = "BayesEstimation";
+            this.Text = "BayesEstimation";
+            this.Load += new System.EventHandler(this.BayesEstimation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart_prior)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_posterior)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_prior;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_posterior;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_PriorVariance;
+        private System.Windows.Forms.TextBox textBox_PriorMean;
+        private System.Windows.Forms.Button button_GeneratePrior;
+    }
+}
---
+++ BayesEstimation.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace 统计图形界面1
+{
+    public partial class BayesEstimation : Form
+    {
+        public BayesEstimation()
+        {
+            InitializeComponent();
+            trackBar1.Maximum = 20;
+            string a = textBox_a.Text;
+            string b = textBox_b.Text;
+            if (a.Trim() != "" && b.Trim() != "" && a != null && b != null)
+            {
+                DrawPrior(Convert.ToDouble(a), Convert.ToDouble(b));
+            }
+        }
+        double Gammln(double value){
+        double [] temp = new double [3];
+        int i;
+        double [] data = new double [] {76.180091729471457, -86.505320329416776, 24.014098240830911, -1.231739572450155, 0.001208650973866179, -0.000005395239384953};
+        temp[0] = value;
+        temp[1] = value + 5.5;
+        temp[1] = temp[1] - (value + 0.5) * Math.Log(temp[1]);
+        temp[2] = 1.0000000001900149;
+        for (i = 0;i < 6;i++){
+            temp[0] += 1;
+            temp[2] += data[i] / temp[0];
+        }
+        temp[0] = Math.Log(2.5066282746310007 * temp[2] / value) - temp[1];
+        return temp[0];
+    }
+    
+    double Gamma_Call(double value) {
+        if (value < 1 ){
+            return Math.Exp(Gammln(value));
+        }
+        else if(value == 1){
+            return 1;
+        }
+        value -= 1;
+        return  value * Gamma_Call(value);
+    }
+    double Gamma(double value ){
+        //概率统计里的gamma函数.注意value的值不要大过171且不能且当value<=0时《sin(value*pi)<>0
+        double  ret = 0;
+        //bool IsTrue;
+        if (value > 171 ){
+            //IsTrue = false;
+            MessageBox.Show("输入伽玛函数的值不可大于171！");
+            }
+        else if (value > 0)
+        {
+            ret = Gamma_Call(value);
+            //MessageBox.Show(ret.ToString());
+        }
+        else
+        {
+            ret = Math.Sin(Math.PI * value);
+
+            if (ret == 0)
+            {
+            }
+            else
+            {
+                ret = Math.PI / ret;
+                ret = ret / Gamma_Call(1 - value);
+            }
+        }
+        return ret;
+    }
+        
+        void DrawPrior(double a, double b)
+        {
+            chart_prior.Series.Clear();
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Spline;
+            series.BorderWidth = 3;
+            series.MarkerSize = 6;
+            series.LegendText = "Beta先验";
+            double MaxValue = 0;
+            //series.Color = Color.Blue;
+            double y = 0;
+            //MessageBox.Show("Gamma(a + b)"+Gamma(a + b));
+            //MessageBox.Show("Gamma(a)" + Gamma(a) + "Gamma(b)" + Gamma(b));
+            double factor = Gamma(a + b) / (Gamma(a) * Gamma(b));
+            for (double  x = 0; x <= 1; x = x + 0.01)
+            {
+                
+                y = factor * Math.Pow(x, a - 1) * Math.Pow(1-x, b - 1);
+                if (x == 0)
+                {
+                    MaxValue = y;
+                }
+                else
+                {
+                    if (MaxValue < y)
+                    {
+                        MaxValue = y;
+                    }
+                }
+                series.Points.AddXY(x, y);
+                //MessageBox.Show("x = " + x.ToString() + " y = " + y.ToString());
+               
+            }
+                chart_prior.Series.Add(series);
+                var XAxis = chart_prior.ChartAreas[0].AxisX;
+                XAxis.Maximum = 1;
+                XAxis.Minimum =0;
+                var YAxis = chart_prior.ChartAreas[0].AxisY;
+                YAxis.Maximum = Math.Ceiling(MaxValue);
+                YAxis.Minimum = 0;
+                textBox_PriorMean.Text = MathV.round((a / (a + b)).ToString(),4,0);
+                textBox_PriorVariance.Text = MathV.round(((a * b) / ((a + b) * (a + b) * (a + b + 1))).ToString(),4,0);
+        }
+        private void BayesEstimation_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button_GeneratePrior_Click(object sender, EventArgs e)
+        {
+            string a = textBox_a.Text;
+            string b = textBox_b.Text;
+            if (a.Trim() != "" && b.Trim() != "" && a != null && b != null)
+            {
+                DrawPrior(Convert.ToDouble(a), Convert.ToDouble(b));
+            }
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int Position = trackBar1.Value;
+            double a = Convert.ToDouble(Position + 1);
+            double b = Convert.ToDouble(20-Position + 1);
+            textBox_a.Text = a.ToString();
+            textBox_b.Text = b.ToString();
+            DrawPrior(a, b);
+        }
+    }
+}
