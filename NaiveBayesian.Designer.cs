6ef6dc84674b329e0d2b5dac75e1a44366418741
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-26 14:54:02 +0800

    add

--- NaiveBayesian.Designer.cs
+++ NaiveBayesian.Designer.cs
@@ -31,13 +31,18 @@
             this.button_refresh = new System.Windows.Forms.Button();
             this.textBox_ChosenCols = new System.Windows.Forms.TextBox();
             this.textBox_result = new System.Windows.Forms.TextBox();
-            this.textBox_Class = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button_GenrateRules = new System.Windows.Forms.Button();
             this.button_predict = new System.Windows.Forms.Button();
             this.dataGridView_subset = new System.Windows.Forms.DataGridView();
             this.button_import = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_cols = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox_Class = new System.Windows.Forms.ComboBox();
+            this.comboBox_output = new System.Windows.Forms.ComboBox();
+            this.button_refresh_predict = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_subset)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,13 +71,6 @@
             this.textBox_result.Size = new System.Drawing.Size(396, 212);
             this.textBox_result.TabIndex = 21;
             // 
-            // textBox_Class
-            // 
-            this.textBox_Class.Location = new System.Drawing.Point(399, 12);
-            this.textBox_Class.Name = "textBox_Class";
-            this.textBox_Class.Size = new System.Drawing.Size(70, 21);
-            this.textBox_Class.TabIndex = 22;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -95,7 +93,7 @@
             // 
             // button_GenrateRules
             // 
-            this.button_GenrateRules.Location = new System.Drawing.Point(503, 12);
+            this.button_GenrateRules.Location = new System.Drawing.Point(552, 12);
             this.button_GenrateRules.Name = "button_GenrateRules";
             this.button_GenrateRules.Size = new System.Drawing.Size(97, 23);
             this.button_GenrateRules.TabIndex = 25;
@@ -105,12 +103,13 @@
             // 
             // button_predict
             // 
-            this.button_predict.Location = new System.Drawing.Point(503, 50);
+            this.button_predict.Location = new System.Drawing.Point(621, 324);
             this.button_predict.Name = "button_predict";
-            this.button_predict.Size = new System.Drawing.Size(97, 23);
+            this.button_predict.Size = new System.Drawing.Size(97, 38);
             this.button_predict.TabIndex = 26;
             this.button_predict.Text = "预测";
             this.button_predict.UseVisualStyleBackColor = true;
+            this.button_predict.Click += new System.EventHandler(this.button_predict_Click);
             // 
             // dataGridView_subset
             // 
@@ -131,18 +130,76 @@
             this.button_import.UseVisualStyleBackColor = true;
             this.button_import.Click += new System.EventHandler(this.button_import_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(35, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "列数：";
+            // 
+            // textBox_cols
+            // 
+            this.textBox_cols.Location = new System.Drawing.Point(107, 327);
+            this.textBox_cols.Name = "textBox_cols";
+            this.textBox_cols.Size = new System.Drawing.Size(214, 21);
+            this.textBox_cols.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(327, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 19);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "输出结果所在列：";
+            // 
+            // comboBox_Class
+            // 
+            this.comboBox_Class.FormattingEnabled = true;
+            this.comboBox_Class.Location = new System.Drawing.Point(400, 12);
+            this.comboBox_Class.Name = "comboBox_Class";
+            this.comboBox_Class.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_Class.TabIndex = 33;
+            // 
+            // comboBox_output
+            // 
+            this.comboBox_output.FormattingEnabled = true;
+            this.comboBox_output.Location = new System.Drawing.Point(494, 324);
+            this.comboBox_output.Name = "comboBox_output";
+            this.comboBox_output.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_output.TabIndex = 34;
+            // 
+            // button_refresh_predict
+            // 
+            this.button_refresh_predict.Location = new System.Drawing.Point(107, 362);
+            this.button_refresh_predict.Name = "button_refresh_predict";
+            this.button_refresh_predict.Size = new System.Drawing.Size(75, 23);
+            this.button_refresh_predict.TabIndex = 35;
+            this.button_refresh_predict.Text = "刷新列数";
+            this.button_refresh_predict.UseVisualStyleBackColor = true;
+            this.button_refresh_predict.Click += new System.EventHandler(this.button_refresh_predict_Click);
+            // 
             // NaiveBayesian
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(723, 313);
+            this.ClientSize = new System.Drawing.Size(720, 397);
+            this.Controls.Add(this.button_refresh_predict);
+            this.Controls.Add(this.comboBox_output);
+            this.Controls.Add(this.comboBox_Class);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_cols);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button_import);
             this.Controls.Add(this.dataGridView_subset);
             this.Controls.Add(this.button_predict);
             this.Controls.Add(this.button_GenrateRules);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox_Class);
             this.Controls.Add(this.textBox_result);
             this.Controls.Add(this.button_refresh);
             this.Controls.Add(this.textBox_ChosenCols);
@@ -159,12 +216,17 @@
         private System.Windows.Forms.Button button_refresh;
         private System.Windows.Forms.TextBox textBox_ChosenCols;
         private System.Windows.Forms.TextBox textBox_result;
-        private System.Windows.Forms.TextBox textBox_Class;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button_GenrateRules;
         private System.Windows.Forms.Button button_predict;
         private System.Windows.Forms.DataGridView dataGridView_subset;
         private System.Windows.Forms.Button button_import;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_cols;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox_Class;
+        private System.Windows.Forms.ComboBox comboBox_output;
+        private System.Windows.Forms.Button button_refresh_predict;
     }
 }
--- NaiveBayesian.cs
+++ NaiveBayesian.cs
@@ -42,18 +42,41 @@ namespace 统计图形界面1
             }
             return counts;
         }
-        static double Search(string Record, string[] Str, double[] Probs)
+        int FindCol(string ID)
         {
-            for (int i = 0; i < Str.Length; i++)
+            for (int i = 0; i < dataGridView_subset.ColumnCount; i++)
             {
-                if (Str[i] == Record)
+                if (dataGridView_subset.Columns[i].Name == ID)
                 {
-                    return Probs[i];
+                    return i;
                 }
             }
             return -1;
         }
-        void BayesClassification()
+        int FindColinForm1(string ID)
+        {
+            for (int i = 0; i < Form1.S.dataGridView1.ColumnCount; i++)
+            {
+                if (Form1.S.dataGridView1.Columns[i].Name == ID)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static string Search(string Record, string[] Str, string[] Probs)
+        {
+            for (int i = 0; i < Str.Length; i++)
+            {
+                //MessageBox.Show(Str[i]);
+                if (Str[i].Trim() == Record.Trim())
+                {
+                    return Probs[i];
+                }
+            }
+            return "-1";
+        }
+        string [][] BayesClassification()
         {
             string[][] RawData = new string[dataGridView_subset.Columns.Count][];
             string[][] Classification = new string[dataGridView_subset.Columns.Count][];
@@ -115,21 +138,21 @@ namespace 统计图形界面1
                     textBox_result.AppendText("\r\n");
                 }
                 string[] AllClassi = new string[ClassCount];
-                double[] EachProbs = new double[ClassCount];
+                string[] EachProbs = new string[ClassCount];
                 ClassCount = 0;
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < dataGridView_subset.Columns.Count; i++)
                 {
                     foreach (string EachStr in Classification[i])
                     {
                         textBox_result.AppendText("P("+EachStr + ") = " + FindElement(EachStr, Data[i]) / Convert.ToDouble(Data[i].Length)+"\r\n");
                         AllClassi[ClassCount] = EachStr;
-                        EachProbs[ClassCount] = FindElement(EachStr, Data[i]) / Convert.ToDouble(Data[i].Length);
+                        EachProbs[ClassCount] = (FindElement(EachStr, Data[i]) / Convert.ToDouble(Data[i].Length)).ToString();
                         ClassCount++;
                     }
                 }
                 double count = 0;
                 int TotalTimes = 0;
-                int InterestCol = Convert.ToInt32(textBox_Class.Text) - 1;
+                int InterestCol = FindCol(comboBox_Class.Text);
                 for (int j = 0; j < dataGridView_subset.Columns.Count; j++)
                 {
                     if (j!= InterestCol)
@@ -157,7 +180,7 @@ namespace 统计图形界面1
                 }
                 textBox_result.AppendText("所有类别组合数量总计：" + TotalTimes.ToString());
                 string[] str = new string[TotalTimes];
-                double[] Probs = new double[TotalTimes];
+                string[] Probs = new string[TotalTimes];
                 int TimesStr = 0;
                 for (int j = 0; j < dataGridView_subset.Columns.Count; j++)
                 {
@@ -178,13 +201,27 @@ namespace 统计图形界面1
                                         }
                                     }
                                 }
+                                //MessageBox.Show("Str = " + EachStr + "|" + EachDis);
                                 str[TimesStr] = EachStr + "|" + EachDis;
-                                Probs[TimesStr] = count / FindElement(EachDis, Data[InterestCol]);
+                                Probs[TimesStr] = (count / FindElement(EachDis, Data[InterestCol])).ToString();
                                 TimesStr++;
                             }
                         }
                     }
                 }
+            string [][] Result= new string [5][];
+            Result[0] = AllClassi;
+            Result[1] = EachProbs;
+            Result[2] = str;
+            
+            Result[3] = Probs;
+            Result[4] = Classification[InterestCol];
+            /*foreach (string SingleStr in Result[3])
+            {
+                MessageBox.Show(SingleStr);
+            }
+            MessageBox.Show("End");*/
+            return Result;
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -312,7 +349,7 @@ namespace 统计图形界面1
             char[] separator = { ',' };
             string UseToAddCol = "";
             int AddTimes = 1;
-            string[] ColWanted =(textBox_ChosenCols.Text+","+textBox_Class.Text).Split(separator);
+            string[] ColWanted =(textBox_ChosenCols.Text).Split(separator);
             if (OriginDT != null)
             {
                 for (int q = 0; q < OriginDT.Rows.Count; q++)
@@ -351,10 +388,131 @@ namespace 统计图形界面1
         {
             textBox_ChosenCols.Text=RenewCol();
         }
+        int IdentifyForm1NARow(int RowsNum, string[] ColsOfVariables)
+        {
+            for (int i = 0; i < Form1.S.dataGridView1.Columns.Count; i++)
+            {
+                if (ColsOfVariables.Contains(i.ToString()) ==true )
+                {
+                    if (Form1.S.dataGridView1.Rows[RowsNum].Cells[i].Value.ToString().Trim() == "")
+                    {
+                        return 0;
+                        //0即为有空格
+                    }
+                }
+            }
+            return 1;
+            //1为没有空格
+        }
+        void NaiveBayesPrediction()
+        {
+            string[][] Information = BayesClassification();
+            /*foreach (string SingleStr in Information[2])
+            {
+                MessageBox.Show(SingleStr);
+            }*/
+            int OutputCol = FindColinForm1(comboBox_output.Text);
+            string SelectedCols = textBox_cols.Text;
+            char[] separator = { ',' };
+            string[] AllCols = SelectedCols.Split(separator);
+            int CountValid = 0;
+            foreach (string EachCol in AllCols)
+            {
+                if (EachCol != "")
+                {
+                    CountValid++;
+                }
+            }
+            string[] ColsOfVariables = new string[CountValid];
+            int Counts = 0;
+            double Product = 1;
+            double[] ForCompare = new double [Information[4].Length];
+            for (int RowsNum = 0 ; RowsNum < Form1.S.dataGridView1.Rows.Count -1;RowsNum ++)
+            {
+                 if (IdentifyForm1NARow(RowsNum,ColsOfVariables) == 1)
+                 {
+                     Counts = 0;
+                     foreach (string EachCol in AllCols)
+                     {
+                         if (EachCol != "" && EachCol != OutputCol.ToString() )
+                         {
+                             ColsOfVariables[Counts] = Form1.S.dataGridView1.Rows[RowsNum].Cells[Convert.ToInt32(EachCol) - 1].Value.ToString();
+                             //MessageBox.Show(ColsOfVariables[Counts]);
+                             Counts++;
+                         }
+                     }
+                     //每读出来一行计算一次
+                     /*Result[0] = AllClassi;
+                      Result[1] = EachProbs;
+                      Result[2] = str;
+                      Result[3] = Probs;
+                      Result[4] = Classification[InterestCol];*/
+                 for (int CountClass = 0; CountClass < Information[4].Length; CountClass++)
+                 {
+                     Product = 1;
+                     for (int i = 0; i < Counts ; i++)
+                     {
+                         //MessageBox.Show(Product.ToString ());
 
+                         Product *= Convert.ToDouble(Search(ColsOfVariables[i].Trim() + "|" + Information[4][CountClass].Trim(), Information[2], Information[3]));
+
+                     }
+                     Product *= Convert.ToDouble(Search(Information[4][CountClass].Trim(), Information[0], Information[1]));
+                     ForCompare[CountClass] = Product;
+                     //MessageBox.Show(Information[4][CountClass]+ForCompare[CountClass].ToString());
+                 }
+                 Form1.S.dataGridView1.Rows[RowsNum].Cells[OutputCol].Value = Information[4][FindMaxPosition(ForCompare)];
+            //double ans = Search("打喷嚏|感冒", str, Probs) * Search("建筑工人|感冒", str, Probs) * Search("感冒", AllClassi, EachProbs) / (Search("打喷嚏", AllClassi, EachProbs) * Search("建筑工人", AllClassi, EachProbs));
+                 }
+                
+            }
+            
+        }
+        int FindMaxPosition(double[] NumberSeries)
+        {
+            double Temp = NumberSeries[0];
+            int MaxPosition = 0;
+            for(int i = 1; i < NumberSeries.Length ;i++)
+            {
+                if (Temp < NumberSeries[i])
+                {
+                    //MessageBox.Show(Temp.ToString());
+                    Temp = NumberSeries[i];
+                    MaxPosition = i;
+                }
+            }
+            return MaxPosition;
+        }
+        void refresh_Combox_Class()
+        {
+            comboBox_Class.Items.Clear();
+            int ColCounts = dataGridView_subset.ColumnCount;
+            for (int i = 0; i < ColCounts; i++)
+            {
+                comboBox_Class.Items.Add(dataGridView_subset.Columns[i].Name);
+            }
+
+        }
         private void button_import_Click(object sender, EventArgs e)
         {
             ImportOriginData();
+            refresh_Combox_Class();
+        }
+
+        private void button_predict_Click(object sender, EventArgs e)
+        {
+            NaiveBayesPrediction();
+        }
+
+        private void button_refresh_predict_Click(object sender, EventArgs e)
+        {
+            textBox_cols.Text = RenewCol();
+            comboBox_output.Items.Clear();
+            int ColCounts = Form1.S.dataGridView1.ColumnCount;
+            for (int i = 0; i < ColCounts; i++)
+            {
+                comboBox_output.Items.Add(Form1.S.dataGridView1.Columns[i].Name);
+            }
         }
     }
 }
