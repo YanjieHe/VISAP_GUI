c8d3bd9bdafb09cf6fa6faa6d81b73b608fe566f
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-01-26 13:45:14 +0800

    add

---
+++ BayesPrediction.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace 统计图形界面1
+{
+    partial class BayesPrediction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_sample = new System.Windows.Forms.TextBox();
+            this.textBox_values = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_predict = new System.Windows.Forms.TextBox();
+            this.button_predict = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "后验分布：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Beta函数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(286, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "参数b：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(286, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "参数a：";
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(381, 13);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 21);
+            this.textBox_a.TabIndex = 4;
+            this.textBox_a.Text = "5";
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(381, 50);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(100, 21);
+            this.textBox_b.TabIndex = 5;
+            this.textBox_b.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(29, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "样本数：";
+            // 
+            // textBox_sample
+            // 
+            this.textBox_sample.Location = new System.Drawing.Point(140, 52);
+            this.textBox_sample.Name = "textBox_sample";
+            this.textBox_sample.Size = new System.Drawing.Size(100, 21);
+            this.textBox_sample.TabIndex = 7;
+            this.textBox_sample.Text = "30";
+            // 
+            // textBox_values
+            // 
+            this.textBox_values.Location = new System.Drawing.Point(140, 91);
+            this.textBox_values.Name = "textBox_values";
+            this.textBox_values.Size = new System.Drawing.Size(121, 21);
+            this.textBox_values.TabIndex = 8;
+            this.textBox_values.Text = "3,4,5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(29, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "待预测值：";
+            // 
+            // textBox_predict
+            // 
+            this.textBox_predict.Location = new System.Drawing.Point(33, 131);
+            this.textBox_predict.Multiline = true;
+            this.textBox_predict.Name = "textBox_predict";
+            this.textBox_predict.Size = new System.Drawing.Size(448, 119);
+            this.textBox_predict.TabIndex = 10;
+            // 
+            // button_predict
+            // 
+            this.button_predict.Location = new System.Drawing.Point(487, 131);
+            this.button_predict.Name = "button_predict";
+            this.button_predict.Size = new System.Drawing.Size(75, 23);
+            this.button_predict.TabIndex = 11;
+            this.button_predict.Text = "预测";
+            this.button_predict.UseVisualStyleBackColor = true;
+            this.button_predict.Click += new System.EventHandler(this.button_predict_Click);
+            // 
+            // BayesPrediction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 262);
+            this.Controls.Add(this.button_predict);
+            this.Controls.Add(this.textBox_predict);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_values);
+            this.Controls.Add(this.textBox_sample);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "BayesPrediction";
+            this.Text = "BayesPrediction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_sample;
+        private System.Windows.Forms.TextBox textBox_values;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_predict;
+        private System.Windows.Forms.Button button_predict;
+    }
+}
---
+++ BayesPrediction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 统计图形界面1
+{
+    public partial class BayesPrediction : Form
+    {
+        public BayesPrediction()
+        {
+            InitializeComponent();
+        }
+
+        void BayesianPrediction()
+        {
+            double a_post = Convert.ToDouble(textBox_a.Text);
+            double b_post = Convert.ToDouble(textBox_b.Text);
+            string AllValues = textBox_values.Text;
+            char[] separator = { ',' };
+            string[] EachValue = AllValues.Split(separator);
+            string AllInformation = "";
+            double PredictProb ;
+            double n = Convert.ToDouble(textBox_sample.Text);
+            double y;
+            double factor = 0;
+            factor = Stat.Gamma(a_post + b_post) / (Stat.Gamma(a_post) * Stat.Gamma(b_post));
+            AllInformation += Form1.S.AdjustStr("成功次数") + "\t" + Form1.S.AdjustStr("预测概率值") + "\r\n";
+            for (int i = 0; i < EachValue.Length; i++)
+            {
+                if (EachValue[i].Trim() != "")
+                {
+                    y = Convert.ToDouble(EachValue[i].Trim());
+                    PredictProb = factor * Stat.Gamma(n + 1) * Stat.Gamma(y + a_post) * Stat.Gamma(n - y + b_post) / (Stat.Gamma(y + 1) * Stat.Gamma(n - y + 1) * Stat.Gamma(a_post + b_post + n));
+                    
+                    AllInformation += Form1.S.AdjustStr(EachValue[i].Trim()) + "\t" + Form1.S.AdjustStr(MathV.NumberPolish(PredictProb.ToString())) + "\r\n";
+                }
+            }
+            textBox_predict.Text = AllInformation;
+
+        }
+        private void button_predict_Click(object sender, EventArgs e)
+        {
+            BayesianPrediction();
+        }
+    }
+}
